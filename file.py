17b2bd9b954c2b057c905ed30da480991b5f216b
muhammad5haris <156824837+muhammad5haris@users.noreply.github.com>
2024-01-16 22:06:33 +0500

    Add files via upload

---
+++ file.py
@@ -0,0 +1,30 @@
+# # task 1 
+# #a=int(input("enter a first number"))
+# #b=int(input("enter a second number"))
+# #c=int(input("enter a third number"))
+# #d=int(input("enter a fourth number"))
+# #e=int(input("enter a fifth number"))
+# #sum=a+b+c+d+e
+# #print(sum)
+
+# #integer
+# integer=4
+# print(integer,"is of type",type(integer))
+
+# #string
+# string="haris"
+# print(string,"is of type",type(string))
+
+# #float
+# float=0.03
+# print(float,"is of type",type(float))
+
+# #bolean
+# bolean=5==5
+# print(bolean,"is of type",type(bolean))
+
+# #set
+# set={1,2,3,4}
+# print(set,"is of type",type(set))
+
+  
